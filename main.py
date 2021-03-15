dab87d668f822e566fbc0bec6d7ecca61b44b141
AndreaTerenz <andrea.terenziani,at@gmail.com>
2021-03-15 18:43:54 +0100

    Migliorato movimento dei paddle

--- main.py
+++ main.py
@@ -13,66 +13,72 @@ from paddle import Paddle
 def main():
     pygame.init()
 
-    #Definiamo colori e dimensioni
-    #size = width, height = 1024, 800
-    size = width, height = (700,500)
+    # Definiamo colori e dimensioni
+    # size = width, height = 1024, 800
+    size = width, height = (700, 500)
     speed = [1, 1]
-    black = (0,0,0)
-    white = (255,255,255)
+    black = (0, 0, 0)
+    white = (255, 255, 255)
 
-    #Definiamo la finestra
+    # Definiamo la finestra
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption("Pong")
-    
-    
+
     paddleA = Paddle(white, 10, 100)
     paddleA.rect.x = 20
     paddleA.rect.y = 200
-    
+
     paddleB = Paddle(white, 10, 100)
     paddleB.rect.x = 670
     paddleB.rect.y = 200
-    #ball = pygame.image.load("intro_ball.gif")
-    #ballrect = ball.get_rect()
-    
-    #Creazione di una lista che memorizza tutti gli sprite che creeremo nel gioco (ossia 2)
+    # ball = pygame.image.load("intro_ball.gif")
+    # ballrect = ball.get_rect()
+
+    # Creazione di una lista che gestisce tutti gli sprite che creeremo nel gioco (i due paddle e la pallina)
     all_sprites_list = pygame.sprite.Group()
     all_sprites_list.add(paddleA)
     all_sprites_list.add(paddleB)
-    
-    
-    #Il loop va avanti finchè l'utente non esce dal gioco 
+
+    # Il loop va avanti finchè l'utente non esce dal gioco
     carryOn = True
-    clock = pygame.time.Clock() #verrà usato per controllare quanto velocemente si aggiorna lo screen
+    clock = pygame.time.Clock()  # verrà usato per controllare quanto velocemente si aggiorna lo screen
 
     while carryOn:
-        
-        #Qui viene gestito il clic sulla "X" che chiude la finestra
+
+        # Qui viene gestito il clic sulla "X" che chiude la finestra
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                carryOn = False
-                pygame.quit()
-                exit()
-                
-            #In questo modo si riesce a spostare il paddle A con le frecce (su e giù)    
-            # if event.type == pygame.KEYDOWN:
-            #     if event.key == pygame.K_UP:
-            #         paddleA.rect.y -= 3
-            #     if event.key == pygame.K_DOWN:
-            #         paddleA.rect.y += 3
-            
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_w]:
-                paddleB.moveUp(6)
-            if keys[pygame.K_s]:
-                paddleB.moveDown(6)
-            if keys[pygame.K_UP]:
-                paddleA.moveUp(6)
-            if keys[pygame.K_DOWN]:
-                paddleA.moveDown(6)
-           
-            
+                quit_game()
 
+            # se viene premuto uno dei tasti di movimento, si dice al paddle corretto di
+            # cominciare a muoversi in quella direzione
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    paddleA.setMovementDir(Paddle.MOVE_UP)
+                if event.key == pygame.K_s:
+                    paddleA.setMovementDir(Paddle.MOVE_DOWN)
+
+                if event.key == pygame.K_i:
+                    paddleB.setMovementDir(Paddle.MOVE_UP)
+                if event.key == pygame.K_k:
+                    paddleB.setMovementDir(Paddle.MOVE_DOWN)
+
+                # altro modo per chiudere
+                if event.key == pygame.K_ESCAPE:
+                    quit_game()
+
+            # se viene rilasciato il tasto corrispondente alla direzione in cui il paddle si sta
+            # muovendo al momento, gli si dice di smettere di muoversi
+            if event.type == pygame.KEYUP:
+                if (event.key == pygame.K_w and paddleA.mov_dir == Paddle.MOVE_UP) or \
+                   (event.key == pygame.K_s and paddleA.mov_dir == Paddle.MOVE_DOWN):
+                    paddleA.setMovementDir(0)
+
+                if (event.key == pygame.K_i and paddleB.mov_dir == Paddle.MOVE_UP) or \
+                   (event.key == pygame.K_k and paddleB.mov_dir == Paddle.MOVE_DOWN):
+                    paddleA.setMovementDir(0)
+
+        # viene chiamato il metodo update() di ogni sprite nel gruppo
         all_sprites_list.update()
 
         # ballrect = ballrect.move(speed)
@@ -84,11 +90,17 @@ def main():
         screen.fill(black)
         pygame.draw.line(screen, white, [349, 0], [349, 500], 5)
         all_sprites_list.draw(screen)
-        #screen.blit(ball, ballrect)
+        # screen.blit(ball, ballrect)
         pygame.display.flip()
-       
-        #Limite a 90 frame per secondo
+
+        # Limite a 90 frame per secondo
         clock.tick(90)
+
+
+def quit_game():
+    pygame.quit()
+    exit()
+
 
 pygame.quit()
 if __name__ == '__main__':
--- paddle.py
+++ paddle.py
@@ -6,34 +6,45 @@ Created on Fri Mar 12 14:40:33 2021
 """
 
 import pygame
-black = (0,0,0)
 
+black = (0, 0, 0)
+
+
+# Rappresenta un paddle
 class Paddle(pygame.sprite.Sprite):
-    #Rappresenta un paddle
-    
+    # attributo statico per indicare la velocità con cui un paddle può muoversi
+    SPEED = 6
+    MOVE_UP = -1
+    MOVE_DOWN = 1
+
     def __init__(self, color, width, height):
         super().__init__()
-        
-        #Setta il colore del background e lo rende trasparente
+
+        # Setta il colore del background e lo rende trasparente
         self.image = pygame.Surface([width, height])
         self.image.fill(black)
+        self.mov_dir = 0
+
         self.image.set_colorkey(black)
-       
-        #Disegna il rettangolo
-        pygame.draw.rect(self.image, color, [0,0, width, height])
-        #Fa fetch con le dimensioni dell'immagine
+        # Disegna il rettangolo
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        # Fa fetch con le dimensioni dell'immagine
         self.rect = self.image.get_rect()
-        
-    #Per muovere su e giù i rettangoli
-    def moveUp(self, pixels):
-        self.rect.y -= pixels
-        #Check che non stia andando fuori dallo screen
-        if self.rect.y < 0:
-            self.rect.y = 0
-            
-    def moveDown(self, pixels):
-        self.rect.y += pixels
-        #Stesso check di prima
-        if self.rect.y > 400:
-            self.rect.y = 400
-       
+
+    # metodo ereditato dalla classe Sprite, chiamato dall'omonimo update() del gruppo in cui
+    # si trova lo sprite
+    # non serve sapere cosa facciano gli argomenti
+    def update(self, *args, **kwargs) -> None:
+        self.rect.y += Paddle.SPEED * self.mov_dir
+        # in questo modo la Y del paddle è sempre almeno 0
+        self.rect.y = max(self.rect.y, 0)
+        # in questo modo la Y del paddle è sempre al massimo 400
+        self.rect.y = min(self.rect.y, 400)
+
+    def setMovementDir(self, dir):
+        if dir == Paddle.MOVE_UP:
+            self.mov_dir = Paddle.MOVE_UP
+        elif dir == Paddle.MOVE_DOWN:
+            self.mov_dir = Paddle.MOVE_DOWN
+        else:
+            self.mov_dir = 0
